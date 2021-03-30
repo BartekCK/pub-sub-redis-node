0fc4ced85b7527ba2a9eb09c69b7d195d21e58c0
Bartlomiej Kotarski <bkotarski1@gmail.com>
2021-03-30 21:57:25 +0200

    filepath

--- subscriber/src/services/FileManager.ts
+++ subscriber/src/services/FileManager.ts
@@ -1,13 +1,16 @@
 import fs from 'fs';
+import { join } from 'path';
 
 export class FileManager {
+    private filePath: string = join('../', this.fileName);
+
     constructor(private fileName: string) {}
 
     clearFile() {
-        fs.truncate(this.fileName, 0, this.handleError);
+        fs.truncate(this.filePath, 0, this.handleError);
     }
     appendToFile(msg: string) {
-        fs.appendFile(this.fileName, `${msg}\n`, this.handleError);
+        fs.appendFile(this.filePath, `${msg}\n`, this.handleError);
     }
 
     private handleError(err: NodeJS.ErrnoException | null): void {
